132fd40a55df2d9ba7aff9f0de20b3cccc2fa641
Mathias Münch <mathias-muench@users.noreply.github.com>
2024-03-17 09:07:35 +0100

    Make it run on Windows

--- xcsoar-convert-cupx.py
+++ xcsoar-convert-cupx.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 
 import csv
-import sys
-import argparse
 import os
 
 # Takes a POINTS.CUP file in cupx format and converts it to a waypoints_details
 # file.
 
-input_file = open("POINTS.CUP", 'r')
+input_file = open("POINTS.CUP", 'r', encoding="UTF-8")
 
 # convert to unix line format
 input_file_content = input_file.read()
 input_file_content = input_file_content.replace('\r\n', '\n')
 input_file_content = input_file_content.replace('\r', '\n')
 
-temp_file = open("POINTS-UNIX.cup", 'w')
+temp_file = open("POINTS-UNIX.cup", 'w', encoding="UTF-8")
 temp_file.write(input_file_content)
 temp_file.close()
 
 cup_unix_file = "POINTS-UNIX.cup"
 
 # Create a corresponding waypoints_details file
-with open(cup_unix_file, 'r') as csv_in_file:
+with open(cup_unix_file, 'r', encoding="UTF-8") as csv_in_file:
     csv_reader = csv.reader(csv_in_file)
-    output_file = open("waypoints_details.txt", 'w')
+    output_file = open("waypoints_details.txt", 'w', encoding="UTF-8")
     for row in csv_reader:
         if row[0] == "name":
             continue
@@ -43,5 +41,5 @@ with open(cup_unix_file, 'r') as csv_in_file:
         # Add newline for better readability
         output_file.write("\n")
     output_file.close()
-    # remove cupx file
-    os.remove(cup_unix_file)
+# remove cupx file
+os.remove(cup_unix_file)
