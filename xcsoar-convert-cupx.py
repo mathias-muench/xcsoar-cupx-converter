365a5bfe0363b2d203e9013a99a5d618146192ff
Mathias Münch <mathias-muench@users.noreply.github.com>
2024-03-17 09:08:11 +0100

    Formatting

--- xcsoar-convert-cupx.py
+++ xcsoar-convert-cupx.py
@@ -6,37 +6,37 @@ import os
 # Takes a POINTS.CUP file in cupx format and converts it to a waypoints_details
 # file.
 
-input_file = open("POINTS.CUP", 'r', encoding="UTF-8")
+input_file = open("POINTS.CUP", "r", encoding="UTF-8")
 
 # convert to unix line format
 input_file_content = input_file.read()
-input_file_content = input_file_content.replace('\r\n', '\n')
-input_file_content = input_file_content.replace('\r', '\n')
+input_file_content = input_file_content.replace("\r\n", "\n")
+input_file_content = input_file_content.replace("\r", "\n")
 
-temp_file = open("POINTS-UNIX.cup", 'w', encoding="UTF-8")
+temp_file = open("POINTS-UNIX.cup", "w", encoding="UTF-8")
 temp_file.write(input_file_content)
 temp_file.close()
 
 cup_unix_file = "POINTS-UNIX.cup"
 
 # Create a corresponding waypoints_details file
-with open(cup_unix_file, 'r', encoding="UTF-8") as csv_in_file:
+with open(cup_unix_file, "r", encoding="UTF-8") as csv_in_file:
     csv_reader = csv.reader(csv_in_file)
-    output_file = open("waypoints_details.txt", 'w', encoding="UTF-8")
+    output_file = open("waypoints_details.txt", "w", encoding="UTF-8")
     for row in csv_reader:
         if row[0] == "name":
             continue
         output_file.write("[" + row[0] + "]\n")
-        if ';' in row[13]:
-            for item in row[13].split(';'):
-                if '.jpg' in item:
+        if ";" in row[13]:
+            for item in row[13].split(";"):
+                if ".jpg" in item:
                     output_file.write("image=pics/" + item + "\n")
-                if '*.pdf' in item:
+                if "*.pdf" in item:
                     output_file.write("file=docs/" + item + "\n")
         else:
-            if '.jpg' in row[13]:
+            if ".jpg" in row[13]:
                 output_file.write("image=pics/" + row[13] + "\n")
-            if '.pdf' in row[13]:
+            if ".pdf" in row[13]:
                 output_file.write("file=docs/" + row[13] + "\n")
         # Add newline for better readability
         output_file.write("\n")
